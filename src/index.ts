7a0ca93e81e587035185eb4163567321369fef3b
Rob Murrer <robmurrer@gmail.com>
2020-09-05 07:14:00 -0700

    fix froggo animation by moving to onload :(

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,5 @@
 import kontra, { init, Sprite, GameLoop, SpriteSheet, imageAssets, initKeys, keyPressed } from 'kontra' 
 
-let { canvas } = init()
-
-
-let frogo = new Image()
-frogo.src = 'assets/images/frog.gif'
-frogo.onload = function() {
-}
-
-let spriteSheet = SpriteSheet({
-	image: frogo,
-	frameWidth: 16,
-	frameHeight: 16,
-	animations: {
-		jump: {
-			frames: '0..1',
-			frameRate: 10, 
-		}
-	}
-})
-
-let sprite = Sprite({
-	x:256,
-	y:256,
-	dy:0,
-	dx:0,
-	animations: spriteSheet.animations
-})
-
-let lefty_state = false;
 interface FGState {
 	p1key_up: boolean,
 	p1key_down: boolean,
@@ -39,6 +10,7 @@ interface FGState {
 }
 
 let state : FGState = {
+
 	p1key_up: false,
 	p1key_down: false,
 	p1key_left: false,
@@ -46,68 +18,90 @@ let state : FGState = {
 	p1key_jump: false,
 };
 
+let { canvas } = init()
 initKeys();
-let loop = GameLoop({
 
-	update: function() {
-		sprite.update()
-
-		
-		if (sprite.y < 0) {
-			sprite.y = 512
+let frogo = new Image()
+frogo.src = 'assets/images/frog.gif'
+frogo.onload = function() {
+	let spriteSheet = SpriteSheet({
+		image: frogo,
+		frameWidth: 16,
+		frameHeight: 16,
+		animations: {
+			jump: {
+				frames: '0..1',
+				frameRate: 3, 
+			}
 		}
+	})
 
-		if (sprite.x < 0) {
-			sprite.x = 512
-		}
+	let sprite = Sprite({
+		x:256,
+		y:256,
+		dy:0,
+		dx:0,
+		animations: spriteSheet.animations
+	})
 
-		if (sprite.y > 512) {
-			sprite.y = 0
-		}
-		if (sprite.x > 512) {
-			sprite.x = 0
-		}
+	let loop = GameLoop({
+		update: function() {
+			if (sprite.y < 0) {
+				sprite.y = 512
+			}
 
-		state.p1key_up = keyPressed('w')
-		state.p1key_left = keyPressed('a')
-		state.p1key_down = keyPressed('s')
-		state.p1key_right = keyPressed('d')
-		state.p1key_jump = keyPressed('space')
+			if (sprite.x < 0) {
+				sprite.x = 512
+			}
 
-		const swp = 3.14
-		if (state.p1key_jump) {
-			sprite.y = sprite.y - swp*swp;
-		}
+			if (sprite.y > 512) {
+				sprite.y = 0
+			}
+			if (sprite.x > 512) {
+				sprite.x = 0
+			}
 
-		if (state.p1key_up) {
-			//sprite.dy = -swp;
-			sprite.y = sprite.y - swp
-		}
+			state.p1key_up = keyPressed('w')
+			state.p1key_left = keyPressed('a')
+			state.p1key_down = keyPressed('s')
+			state.p1key_right = keyPressed('d')
+			state.p1key_jump = keyPressed('space')
 
-		if (state.p1key_down) {
-			//sprite.dy = swp;
-			sprite.y = sprite.y + swp
-		}
-		if (state.p1key_left) {
-			//sprite.dx = -swp;
-			sprite.x = sprite.x - swp
-		}
+			const swp = 3.14
+			if (state.p1key_jump) {
+				sprite.y = sprite.y - swp*swp;
+			}
 
-		if (state.p1key_right) {
-			//sprite.dx = swp;
-			sprite.x = sprite.x + swp
-		}
+			if (state.p1key_up) {
+				//sprite.dy = -swp;
+				sprite.y = sprite.y - swp
+			}
 
+			if (state.p1key_down) {
+				//sprite.dy = swp;
+				sprite.y = sprite.y + swp
+			}
+			if (state.p1key_left) {
+				//sprite.dx = -swp;
+				sprite.x = sprite.x - swp
+			}
 
-		//sprite.playAnimation('jump');
+			if (state.p1key_right) {
+				//sprite.dx = swp;
+				sprite.x = sprite.x + swp
+			}
 
-	},
+			sprite.update()
 
-	render: function() {
-		sprite.render()
-	}
-})
+		},
 
-loop.start();
+		render: function() {
+			//sprite.playAnimation('jump');
+			sprite.render()
+		},
+	})
+
+	loop.start();
+}
 
 
